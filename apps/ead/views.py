a5171cfb226f6dc882ab258054940082813949c9
Andre-Britto <andrebritto79@hotmail.com>
2020-12-14 13:59:06 -0300

    condição views ead

--- apps/ead/views.py
+++ apps/ead/views.py
@@ -1,19 +1,24 @@
 import datetime
 from django.shortcuts import render
 from apps.ead.models import Curso
+from django.contrib.auth.decorators import login_required
+from apps.ead.models import Pessoa
 
 
+@login_required(login_url='/accounts/login/')
 def ead(request):
     user = getattr(request, 'user', None)
-    if user.projeto.ead == "EAD":
+    professor = Pessoa.objects.filter(user_id=user.id) and Pessoa.objects.filter(acesso=1)
+    if professor:
         curso = Curso.objects.filter(data_inicio__lte=datetime.date.today())
         ano = datetime.date.today()
 
         context = {
             'user': user,
             'ano': ano,
-            'cursos': curso
+            'cursos': curso,
+            'professores': professor
         }
         return render(request, 'ead/ead_dashboard.html', context)
     else:
-        return render(request, 'home/permissao_negada.html')
+        return render(request, 'ead/ead_home.html')
